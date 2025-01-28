4dc3a5579bc9e062b0925c4b4f9b2a249d2660f1
Shoney <shoney@writeonc.de>
2025-01-28 20:39:33 +0100

    secure post and delete

--- src/api/valid.rs
+++ src/api/valid.rs
@@ -15,3 +15,19 @@ pub async fn validate_token(req: &HttpRequest) -> Result<(), HttpResponse> {
     // If token is invalid or not present, return Unauthorized
     Err(HttpResponse::Unauthorized().finish())
 }
+
+pub async fn validate_admin_token(req: &HttpRequest) -> Result<(), HttpResponse> {
+    // Get the token from the authorization header
+    if let Some(auth_header) = req.headers().get("Authorization") {
+        if let Ok(token) = auth_header.to_str() {
+            
+            let api_token = config::get_api_access_admin_token();
+            if token.trim_start_matches("Bearer ") == api_token {
+                return Ok(());
+            }
+        }
+    }
+
+    // If token is invalid or not present, return Unauthorized
+    Err(HttpResponse::Unauthorized().finish())
+}
