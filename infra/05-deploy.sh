1db0f6775dfa49ab29541c97751166ef96133cd3
Shoney <shoney@writeonce.de>
2025-02-27 13:20:29 +0100

    update

--- infra/05-deploy.sh
+++ infra/05-deploy.sh
@@ -2,7 +2,7 @@
 ssh -v -i /tmp/remote-login-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/tmp/known_hosts "$SERVER_USER@$SERVER_DNS_OR_IP" << EOF
   mkdir -p ~/.docker && echo "$DOCKER_AUTH_CONFIG" > ~/.docker/config.json
   docker pull "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
-  docker stop $CONTAINER_NAME || true  # Stop any running instance
-  docker rm $CONTAINER_NAME || true    # Remove stopped instance
-  docker run -d --name $CONTAINER_NAME -p 80:8080 "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
+  docker stop "$CI_REGISTRY_IMAGE-container" || true  # Stop any running instance
+  docker rm "$CI_REGISTRY_IMAGE-container" || true    # Remove stopped instance
+  docker run -d --name "$CI_REGISTRY_IMAGE-container" -p $API_PORT:$API_PORT --network writeonce-network "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
 EOF
