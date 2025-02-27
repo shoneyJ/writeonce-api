60e177f1e1813de4f999ceaa9e0a89342cf83add
Shoney <shoney@writeonce.de>
2025-02-27 15:21:01 +0100

    fix: Invalid container name

--- infra/05-deploy.sh
+++ infra/05-deploy.sh
@@ -2,7 +2,7 @@
 ssh -v -i /tmp/remote-login-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/tmp/known_hosts "$SERVER_USER@$SERVER_DNS_OR_IP" << EOF
 
   docker pull "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
-  docker stop "$CI_REGISTRY_IMAGE-container" || true  # Stop any running instance
-  docker rm "$CI_REGISTRY_IMAGE-container" || true    # Remove stopped instance
-  docker run -d --name "$CI_REGISTRY_IMAGE-container" -p $API_PORT:$API_PORT --network writeonce-network "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
+  docker stop "$CI_PROJECT_NAME" || true  # Stop any running instance
+  docker rm "$CI_PROJECT_NAME" || true    # Remove stopped instance
+  docker run -d --name "$CI_PROJECT_NAME" -p $API_PORT:$API_PORT --network writeonce-network "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
 EOF
