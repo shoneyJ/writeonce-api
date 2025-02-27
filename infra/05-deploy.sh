118d09917a18ca4f4542361d625e290388b6918c
Shoney <shoney@writeonce.de>
2025-02-27 13:44:14 +0100

    test

--- infra/05-deploy.sh
+++ infra/05-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ssh -v -i /tmp/remote-login-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/tmp/known_hosts "$SERVER_USER@$SERVER_DNS_OR_IP" << EOF
-  mkdir -p ~/.docker && echo "$DOCKER_AUTH_CONFIG" > ~/.docker/config.json
+
   docker pull "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA"
   docker stop "$CI_REGISTRY_IMAGE-container" || true  # Stop any running instance
   docker rm "$CI_REGISTRY_IMAGE-container" || true    # Remove stopped instance
