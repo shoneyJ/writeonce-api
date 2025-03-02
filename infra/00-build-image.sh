729932a985d962d2a705ed8ce92d40cc3f18445a
Shoney <shoney@writeonce.de>
2025-03-02 20:33:05 +0100

    test

--- infra/00-build-image.sh
+++ infra/00-build-image.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$API_PORT" ] || [ -z "$AWS_INFRA_BASE_URL" ] || [ -z "$DATABASE_URL" ] || [ -z "$API_ACCESS_TOKEN" ] || [ -z "$API_ACCESS_ADMIN_TOKEN" ]; then
   echo "Error: Missing required environment variables"
+  echo "$API_PORT"
   exit 1
 fi
 
