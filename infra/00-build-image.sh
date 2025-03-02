9494361e4a3a64bd7723a273a4a5e5a714a1c3e8
Shoney <shoney@writeonce.de>
2025-03-02 20:21:57 +0100

    update build images

--- infra/00-build-image.sh
+++ infra/00-build-image.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if [ -z "$API_PORT" ] || [ -z "$AWS_INFRA_BASE_URL" ] || [ -z "$DATABASE_URL" ] || [ -z "$API_ACCESS_TOKEN" ] || [ -z "$API_ACCESS_ADMIN_TOKEN" ]; then
+  echo "Error: Missing required environment variables"
+  exit 1
+fi
+
 docker build \
- --build-arg PORT=$API_PORT \
- --build-arg AWS_INFRA_BASE_URL=$AWS_INFRA_BASE_URL \
- --build-arg DATABASE_URL=$DATABASE_URL \
- --build-arg API_ACCESS_TOKEN=$API_ACCESS_TOKEN \
- --build-arg API_ACCESS_ADMIN_TOKEN=$API_ACCESS_ADMIN_TOKEN \
+ --build-arg PORT="$API_PORT" \
+ --build-arg AWS_INFRA_BASE_URL="$AWS_INFRA_BASE_URL" \
+ --build-arg DATABASE_URL="$DATABASE_URL" \
+ --build-arg API_ACCESS_TOKEN="$API_ACCESS_TOKEN" \
+ --build-arg API_ACCESS_ADMIN_TOKEN="$API_ACCESS_ADMIN_TOKEN" \
  -t "$CI_REGISTRY_IMAGE:$CI_COMMIT_SHORT_SHA" .
